feddc3eae404ffbeab755d6ec4bb4dd938f21502
TwoTicks <adityasiddheshwar.adisid@gmail.com>
2021-02-25 18:10:36 +0530

    input output system for competetive

---
+++ c++/input_output.cpp
@@ -0,0 +1,24 @@
+// Below is C/C++ code for input/output 
+#include<iostream> 
+using namespace std;
+int main() 
+{ 
+#ifndef ONLINE_JUDGE 
+
+	// For getting input from input.txt file 
+	freopen("input.txt", "r", stdin); 
+
+	// Printing the Output to output.txt file 
+	freopen("output.txt", "w", stdout); 
+
+#endif 
+int T,A,B;
+cin>>T;
+while(T--)
+{
+    cin>>A>>B;
+    cout<<A+B<<"\n";
+}
+//cout<<"Yeah!"<<x;
+	return 0; 
+} 

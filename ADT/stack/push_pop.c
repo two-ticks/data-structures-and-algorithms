12ba99437bd751894f1bfd3d6e404ca6fdb2ca3a
TwoTicks <adityasiddheshwar.adisid@gmail.com>
2021-03-27 00:03:34 +0530

    format push_pop

--- ADT/stack/push_pop.c
+++ ADT/stack/push_pop.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-//LIFO 
-struct stack{
+
+//LIFO
+
+struct stack
+{
     int size;
     int top;
-    int * arr;
+    int *arr;
 };
 
 int isEmpty(struct stack *ptr)
@@ -25,28 +28,32 @@ int isFull(struct stack *ptr)
     return 0;
 }
 
-void push(struct stack * ptr, int val)
+void push(struct stack *ptr, int val)
 {
-    if (isFull(ptr)){
+    if (isFull(ptr))
+    {
         printf("Stack Overflow");
     }
-    else{
+    else
+    {
         ptr->top++;
-        ptr->arr[ptr->top]=val;
-    } 
+        ptr->arr[ptr->top] = val;
+    }
 }
 
-int pop(struct stack * ptr)
+int pop(struct stack *ptr)
 {
-    if (isEmpty(ptr)){
+    if (isEmpty(ptr))
+    {
         printf("Stack Underflow");
         return -1;
     }
-    else{
+    else
+    {
         int val = ptr->arr[ptr->top];
         ptr->top--;
         return val;
-    } 
+    }
 }
 
 int main()
@@ -57,7 +64,7 @@ int main()
     sp->top = -1;
     sp->arr = (int *)malloc(sp->size * sizeof(int));
     printf("Stack created!");
-    push(sp,5); 
+    push(sp, 5);
     //printf("%d\n",isFull(sp));
     //printf("%d",isEmpty(sp));
     //printf("%d",pop(sp));

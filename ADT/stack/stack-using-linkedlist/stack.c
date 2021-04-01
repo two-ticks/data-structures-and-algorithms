2582517a996c5ff3150682e720af9f1c14bf8ca4
TwoTicks <adityasiddheshwar.adisid@gmail.com>
2021-04-02 00:17:06 +0530

    isEmpty()

--- ADT/stack/stack-using-linkedlist/stack.c
+++ ADT/stack/stack-using-linkedlist/stack.c
@@ -6,7 +6,28 @@ struct Node {
 
 };
 
+void push(struct Node*top, int x){
+
+}
+
+void isEmpty(struct Node* top){
+    if(top == NULL)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+void isFull(struct Node* top){
+
+}
+
+
 int main(){
 
+struct Node * top = NULL;
+
+//push(top, 78);
 return 0
 }

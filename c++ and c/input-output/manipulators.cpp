577a663478474017f035618fde1871e88462c320
TwoTicks <adityasiddheshwar.adisid@gmail.com>
2021-04-16 00:06:38 +0530

    formatting

--- c++ and c/input-output/manipulators.cpp
+++ c++ and c/input-output/manipulators.cpp
@@ -4,34 +4,34 @@ using namespace std;
 
 int main()
 {
-	double A = 100;
-	double B = 2001.5251;
-	double C = 201455.2646;
+    double A = 100;
+    double B = 2001.5251;
+    double C = 201455.2646;
 
-	// We can use setbase(16) here instead of hex
+    // We can use setbase(16) here instead of hex
 
-	// formatting
-	cout << hex << left << showbase << nouppercase;
+    // formatting
+    cout << hex << left << showbase << nouppercase;
 
-	// actual printed part
-	cout << (long long)A << endl;
+    // actual printed part
+    cout << (long long)A << endl;
 
-	// We can use dec here instead of setbase(10)
+    // We can use dec here instead of setbase(10)
 
-	// formatting
-	cout << setbase(10) << right << setw(15)
-		<< setfill('_') << showpos
-		<< fixed << setprecision(2);
+    // formatting
+    cout << setbase(10) << right << setw(15)
+         << setfill('_') << showpos
+         << fixed << setprecision(2);
 
-	// actual printed part
-	cout << B << endl;
+    // actual printed part
+    cout << B << endl;
 
-	// formatting
-	cout << scientific << uppercase
-		<< noshowpos << setprecision(9);
+    // formatting
+    cout << scientific << uppercase
+         << noshowpos << setprecision(9);
 
-	// actual printed part
-	cout << C << endl;
+    // actual printed part
+    cout << C << endl;
 
     return 0;
 }

7fa2e649d023d5a69f7574b3d76efb8a7582a69c
TwoTicks <adityasiddheshwar.adisid@gmail.com>
2021-04-15 22:19:59 +0530

    enum formatted

--- c++ and c/OOP/enumeration.cpp
+++ c++ and c/OOP/enumeration.cpp
@@ -1,10 +1,19 @@
-#include <iostream>  
-using namespace std;  
-enum week { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };  
-int main()  
-{  
-    week day;  
-    day = Friday;  
-    cout << "Day: " << day+1<<endl;  
-    return 0;  
-} 
+#include <iostream>
+using namespace std;
+enum week
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+};
+int main()
+{
+    week day;
+    day = Friday;
+    cout << "Day: " << day + 1 << endl;
+    return 0;
+}

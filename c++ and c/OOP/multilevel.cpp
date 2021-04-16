e62a8a7f757c8c6e3db2fe1d927c8c376a1e559c
TwoTicks <adityasiddheshwar.adisid@gmail.com>
2021-04-16 12:16:19 +0530

    inheritance

---
+++ c++ and c/OOP/multilevel.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class Animal
+{
+public:
+    void eat()
+    {
+        cout << "Eating" << endl;
+    }
+};
+class Dog : public Animal
+{
+public:
+    void bark()
+    {
+        cout << "Barking" << endl;
+    }
+};
+class BabyDog : public Dog
+{
+public:
+    void weep()
+    {
+        cout << "Weeping";
+    }
+};
+int main(void)
+{
+    BabyDog d;
+    d.eat();
+    d.bark();
+    d.weep();
+    return 0;
+}

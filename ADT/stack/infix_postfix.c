34095ac3896853f61ca44049abf04412b450a6a1
two-ticks <adityasiddheshwar.adisid@gmail.com>
2021-06-18 18:30:28 +0530

    docs: prefix and postfix

---
+++ ADT/stack/infix_postfix.c
@@ -0,0 +1,9 @@
+// infix to prefix  x - y * z
+// parenthesise and then start from inner most bracket   (x - (y * z))
+// (x - [*yz])
+// -x * yz
+
+// infix to postfix  x - y * z
+// parenthesise and then start from inner most bracket   (x - (y * z))
+// (x - [yz*])
+// xyz*-

18c8dff3fb13a0135b003da75df9688ea65f607d
two-ticks <adityasiddheshwar.adisid@gmail.com>
2021-06-18 23:02:19 +0530

    infix to postfix

--- ADT/stack/infix_postfix.c
+++ ADT/stack/infix_postfix.c
@@ -7,3 +7,164 @@
 // parenthesise and then start from inner most bracket   (x - (y * z))
 // (x - [yz*])
 // xyz*-
+
+// infix to postfix using stack -> push operators in stack with precedence e.g. (+, 2) '+' is operator and 2 is its precedence
+// pop and add to expression if precendence of operator in expression is less than or equal operator in stack
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct stack
+{
+    int size;
+    int top;
+    char *arr;
+};
+
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void push(struct stack *ptr, int val)
+{
+    if (isFull(ptr))
+    {
+        printf("Stack Overflow");
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = val;
+    }
+}
+
+char pop(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+    {
+        printf("Stack Underflow");
+        return -1;
+    }
+    else
+    {
+        char val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+int match(char a, char b)
+{
+    if (a == '(' && b == ')')
+    {
+        return 1;
+    }
+    else if (a == '[' && b == ']')
+    {
+        return 1;
+    }
+    else if (a == '{' && b == '}')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int stackTop(struct stack *sp)
+{
+    return sp->arr[sp->top];
+}
+int precedence(char ch)
+{
+    if (ch == '*' || ch == '/')
+    {
+        return 3;
+    }
+    else if (ch == '+' || ch == '-')
+    {
+        return 2;
+    }
+    else
+        return 0;
+}
+int isOperator(char ch)
+{
+    if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
+    {
+        return 1;
+    }
+    else
+        return 0;
+}
+char *infixToPostfix(char *infix)
+{
+
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = 100;
+    sp->top = -1;
+    sp->arr = (char *)malloc(sp->size * sizeof(char));
+    char *postfix = (char *)malloc((strlen(infix) + 1) * sizeof(char));
+    int i = 0; //track infix traversal
+    int j = 0; //track postfix addition
+    while (infix[i] != '\0')
+    {
+        if (!isOperator(infix[i]))
+        {
+            postfix[j] = infix[i];
+            j++;
+            i++;
+        }
+        else
+        {
+            if (precedence(infix[i]) > precedence(stackTop(sp)))
+            {
+                push(sp, infix[i]);
+                i++;
+            }
+            else
+            {
+                postfix[j] = pop(sp);
+                j++;
+            }
+        }
+    }
+    while (!isEmpty(sp))
+    {
+        postfix[j] = pop(sp);
+        j++;
+    }
+    postfix[j] = '\0';
+    return postfix;
+}
+
+int main()
+{
+
+    char *infix = "a-b+t/6"; //expression to be checked
+    printf("postfix: %s", infixToPostfix(infix));
+
+    return 0;
+}
